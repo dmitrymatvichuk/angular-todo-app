55107ab396b5ab34022214c0aac432b735a6f7ae
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-08-04 16:37:58 +0300

    Creating layout and basic logic of fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Todo } from './types/todo';
+
+const todos = [
+  { id: 1, title: 'HTML + CSS', completed: true },
+  { id: 2, title: 'JS', completed: false },
+  { id: 3, title: 'React', completed: false },
+  { id: 4, title: 'Angular', completed: false },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  todos = todos;
+
+  todoForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  }
+
+  get activeTodos() {
+    return this.todos.filter((todo) => !todo.completed);
+  }
+
+  addTodo() {
+    if (this.todoForm.invalid) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.title.value,
+      completed: false,
+    };
+
+    this.todos.push(newTodo);
+    this.todoForm.reset();
+  }
 }
